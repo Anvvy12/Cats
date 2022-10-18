925f9aca70483014d43d67cea8cda0ee40db74b3
Bogdan <youbody7@gmail.com>
2022-10-18 11:43:13 +0300

    s

--- src/Cat.jsx
+++ src/Cat.jsx
@@ -5,11 +5,13 @@ const Cat = ({ cat }) => {
   const correntClass = cat.available ? 'cat-card' : 'cat-card_notAvailable';
   return (
     <li className={`${correntClass}`}>
-      <div className="cat-card__price">🔖 For sale {cat.price}</div>
+      <div className="cat-card__price">🔖 For sale: ${cat.price}</div>
       <img src={cat.image_url} className="cat-card__img"></img>
       <div className="cat-card__description ">
-        <span className="cat-card__name">{cat.name} </span>
-        <span className="cat-card__category">{cat.category}</span>
+        <span className="cat-card__name">
+          Name: {cat.name} <br />
+        </span>
+        <span className="cat-card__category">Category: {cat.category}</span>
       </div>
     </li>
   );
--- src/ShowCats.jsx
+++ src/ShowCats.jsx
@@ -3,14 +3,6 @@ import './styles/cats-list.scss';
 import Cat from './Cat';
 
 const ShowCats = ({ cats }) => {
-  //   const [cats, setCats] = useState([]);
-
-  //   useEffect(() => {
-  //     fetch('https://ftl-cryptokitties.fly.dev/api/crypto_kitties')
-  //       .then(response => response.json())
-  //       .then(res => setCats(res.cats));
-  //   }, []);
-
   return (
     <>
       <ol className="cats-list">
